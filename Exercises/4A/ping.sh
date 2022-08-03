ed6d3db258d71531e01fb979ed89b5aa9c436cc7
Giuseppe Pitruzzella <giuseppepitru@icloud.com>
2022-08-03 11:26:54 +0200

    fix: ip address

--- Exercises/4A/ping.sh
+++ Exercises/4A/ping.sh
@@ -1,11 +1,11 @@
 echo "Ping A -> D"
-VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.209.255.100
+VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.210.0.1
 
 echo "Ping C -> G"
-VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.207.255.100
+VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.192.0.1
 
 echo "Ping D -> C"
-VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.208.255.100
+VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.209.0.1
 
 echo "Ping F -> B"
-VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.159.255.100
+VBoxManage guestcontrol A run --exe /bin/ping --username root --password root  --wait-stdout -- ping/ -c 4 10.128.0.2
